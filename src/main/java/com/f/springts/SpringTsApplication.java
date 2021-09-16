c800b8931d07052fd3841d6c102305bc051d07f1
Freddy Inga <freddy.ingaec@gmail.com>
2021-09-16 10:47:25 -0500

    Spring Boot project creation

---
+++ src/main/java/com/f/springts/SpringTsApplication.java
@@ -0,0 +1,13 @@
+package com.f.springts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/f/springts/SpringTsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.f.springts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
